371ce1be8ba011741bc5e0fb4f3b4ac9b2255654
Ali Majidi <amajidi1382@gmail.com>
2024-03-14 22:44:44 +0330

    feat(binary-search): JS file

---
+++ 1. Binary Search/binary-search.js
@@ -0,0 +1,22 @@
+function binary_search(array, item) {
+    let low = 0;
+    let high = array.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let guess = array[mid];
+
+        if (guess < item) {
+            low = mid++;
+        } else if (guess > item) {
+            high = mid--
+        } else {
+            return mid;
+        }
+    }
+
+    return null;
+}
+
+const array = Array.from(Array(100).keys()).map(x => ++x);
+console.log(binary_search(array, 99));
