a19c19d2f606508770293d7fb85a8847eeddbb7b
Ali Majidi <amajidi1382@gmail.com>
2024-03-14 21:55:35 +0330

    feat(binary-search): python file

---
+++ 1. Binary Search/binary-search.py
@@ -0,0 +1,20 @@
+def binary_search(array, item):
+    low = 0
+    high = len(array) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = array[mid]
+
+        if guess < item:
+            low = mid + 1
+        elif guess > item:
+            high = mid - 1
+        else:
+            return mid
+
+    return None
+
+
+array = [i for i in range(1, 101)]
+print(binary_search(array, 99))
